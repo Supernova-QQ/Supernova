1903ef5e1dc21798418fe0104b64852a03fd17f4
Haneol Oh <otyvs1999@gmail.com>
2024-08-12 10:04:05 +0900

    initial commit (#1)

---
+++ src/main/java/com/hanshin/supernova/SupernovaApplication.java
@@ -0,0 +1,13 @@
+package com.hanshin.supernova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupernovaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupernovaApplication.class, args);
+	}
+
+}
