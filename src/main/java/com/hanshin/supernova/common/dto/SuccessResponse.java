0a32295fdb7086ebe57bee635e857840c1fb64f1
OhHaneol <otyvs1109@hs.ac.kr>
2024-08-15 09:33:20 +0900

    feat: add response settings

---
+++ src/main/java/com/hanshin/supernova/common/dto/SuccessResponse.java
@@ -0,0 +1,11 @@
+package com.hanshin.supernova.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SuccessResponse {
+
+    private String message;
+}
