c456d9aa56af5d5f6a4fbd965fe09437aed7f56a
Yewon Park <ywpark0124@naver.com>
2024-08-26 01:05:48 +0900

    feat: add auth logic

---
+++ src/main/java/com/hanshin/supernova/auth/dto/response/AuthLoginResponse.java
@@ -0,0 +1,12 @@
+package com.hanshin.supernova.auth.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthLoginResponse {
+
+    private String nickname;
+    private String token;
+}
