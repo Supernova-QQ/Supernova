f9a6c6f2a3ca5ea7d5d4bd4e850f822b21548bec
Haneol Oh <otyvs1999@gmail.com>
2024-10-10 00:40:27 +0900

    feat: 공지사항 crud + L (#45)
    * feat: add notice exception
    
    * feat: add notice crud logic
    
    * feat: add missing WebMvcConfigurer
    
    * feat: add notice list logic

--- src/main/java/com/hanshin/supernova/config/web/WebConfig.java
+++ src/main/java/com/hanshin/supernova/config/web/WebConfig.java
@@ -1,4 +1,20 @@
 package com.hanshin.supernova.config.web;
 
-public class WebConfig {
+import com.hanshin.supernova.config.resolver.UserArgumentResolver;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final UserArgumentResolver userArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userArgumentResolver);
+    }
 }
