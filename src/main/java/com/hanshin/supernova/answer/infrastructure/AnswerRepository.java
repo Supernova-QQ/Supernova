8a81541cac92ba78abca29336cc61fd2d98bf36e
Yewon Park <ywpark0124@naver.com>
2024-11-13 23:59:35 +0900

    fix: Modify conflict and Run Error(Requires creation of My.html for checking bookmark service)

--- src/main/java/com/hanshin/supernova/answer/infrastructure/AnswerRepository.java
+++ src/main/java/com/hanshin/supernova/answer/infrastructure/AnswerRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
