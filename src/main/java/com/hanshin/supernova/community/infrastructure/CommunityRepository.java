52396c7084dccaa1419319a03aa80869018ef261
Yewon Park <ywpark0124@naver.com>
2024-11-30 01:32:41 +0900

    feat: merge develop

--- src/main/java/com/hanshin/supernova/community/infrastructure/CommunityRepository.java
+++ src/main/java/com/hanshin/supernova/community/infrastructure/CommunityRepository.java
@@ -4,6 +4,8 @@ import com.hanshin.supernova.community.domain.Community;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -26,4 +28,10 @@ public interface CommunityRepository extends JpaRepository<Community, Long> {
     // 커뮤니티 ID로 이미지 URL 조회
     @Query("SELECT c.imgUrl FROM Community c WHERE c.id = :communityId")
     Optional<String> findImgUrlById(@Param("communityId") Long communityId);
+
+    @Query("SELECT c FROM Community c ORDER BY c.createdAt DESC")
+    Page<Community> findAllOrderByCreatedAtDesc(Pageable pageable);
+
+    @Query("SELECT c FROM Community c ORDER BY c.createdAt ASC")
+    Page<Community> findAllOrderByCreatedAtASC(Pageable pageable);
 }
