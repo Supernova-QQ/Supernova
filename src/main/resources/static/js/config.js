90f25751d74a10407a9816349160f85e537e2291
Haneol Oh <otyvs1999@gmail.com>
2025-03-01 00:46:19 +0900

    Revert "feat: 도메인 추가하면서 외부 80포트를 포워딩 2 (#111)"
    This reverts commit bc07e4933c02fdda5d6232ef54c1fe4239ab47b1.

--- src/main/resources/static/js/config.js
+++ src/main/resources/static/js/config.js
@@ -4,8 +4,7 @@ console.log("config script loaded");
 let CONFIG = {
   API: {
     // BASE_URL: 'http://localhost:8080',
-    // BASE_URL: 'http://13.124.80.116:8080',
-    BASE_URL: 'http://13.124.80.116',
+    BASE_URL: 'http://13.124.80.116:8080',
     ENDPOINTS: {
       COMMUNITIES: '/api/communities',
       QUESTIONS: '/api/questions',
