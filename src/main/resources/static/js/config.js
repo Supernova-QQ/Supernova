8c9e702efabca238a92e65b930d43da10ba82eca
OhHaneol <otyvs1109@hs.ac.kr>
2025-03-01 00:38:49 +0900

    feat: connect through port 80 of the host

--- src/main/resources/static/js/config.js
+++ src/main/resources/static/js/config.js
@@ -4,7 +4,8 @@ console.log("config script loaded");
 let CONFIG = {
   API: {
     // BASE_URL: 'http://localhost:8080',
-    BASE_URL: 'http://13.124.80.116:8080',
+    // BASE_URL: 'http://13.124.80.116:8080',
+    BASE_URL: 'http://13.124.80.116',
     ENDPOINTS: {
       COMMUNITIES: '/api/communities',
       QUESTIONS: '/api/questions',
